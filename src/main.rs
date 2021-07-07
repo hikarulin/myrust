09dc89c29eb5da0466ec4a742b1c3959d048a102
linqh1 <qihua.lin@quantil.com>
2021-07-07 16:31:44 +0800

    代码简化

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use clap::{Arg, App};
-use std::collections::HashMap;
-use std::time::{SystemTime, Instant};
-use crypto::mac::Mac;
-use crypto::hmac::Hmac;
-use crypto::sha1::Sha1;
 use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
+
+use clap::{App, Arg};
+use crypto::hmac::Hmac;
+use crypto::mac::Mac;
+use crypto::sha1::Sha1;
 
 static DATA_PATTERN: &str = "%a, %d %b %Y %H:%M:%S GMT";
 
@@ -34,16 +34,17 @@ fn main() {
 
 fn encrypt(user: &str, key: &str) -> HashMap<String, String, RandomState> {
     let now = chrono::prelude::Utc::now().format(DATA_PATTERN).to_string();
+    let encrypt = hmac_sha1(key.as_bytes(),now.as_bytes());
+    let mut authorization_str = String::from(user) + ":" + encrypt.as_str();
+    authorization_str = base64::encode(authorization_str.as_bytes());
+
     let mut headers = HashMap::new();
-    let encrypt = hmacSha1(key.as_bytes(),now.as_bytes());
-    let mut authorizationStr = String::from(user) + ":" + encrypt.as_str();
-    authorizationStr = base64::encode(authorizationStr.as_bytes());
     headers.insert(String::from("Date"),now);
-    headers.insert(String::from("Authorization"),"Basic: ".to_owned() + &authorizationStr);
+    headers.insert(String::from("Authorization"),"Basic: ".to_owned() + &authorization_str);
     return headers;
 }
 
-fn hmacSha1(key: &[u8],value: &[u8]) -> String {
+fn hmac_sha1(key: &[u8],value: &[u8]) -> String {
     let mut hmac1 = Hmac::new(Sha1::new(), key);
     hmac1.input(value);
     base64::encode(hmac1.result().code())
