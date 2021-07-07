5c724a104143c9e5e1ac3cc39ff85276282b3fc9
linqh1 <qihua.lin@quantil.com>
2021-07-07 14:21:27 +0800

    添加依赖:clap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-mod map;
-mod file;
-mod str;
-mod mache;
-
-use std::collections::HashMap;
+extern crate clap;
+use clap::{Arg, App};
 
 fn main() {
-    mache::match1()
+    let matches = App::new("cas-rust")
+        .version("0.1")
+        .args(&[
+            Arg::from_usage("-k, --key <api-key> 'api key'").required_unless("file"),
+            Arg::from_usage("-u, --user <api-user> 'api user'").required_unless("file"),
+            Arg::from_usage("-f, --file [input] 'an optional input file to use'").required_unless_all(&["key","user"])
+        ])
+        .get_matches();
+
+    let key = matches.value_of("key");
+    match key {
+        None => println!("not found arg: key"),
+        Some(s) => println!("args key={}",s)
+    }
 }
