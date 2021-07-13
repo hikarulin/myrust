fdf9cdb943b1739e01be396a8edec963b3c806b5
linqh1 <qihua.lin@quantil.com>
2021-07-13 17:45:33 +0800

    trait示例2

--- src/main.rs
+++ src/main.rs
@@ -68,3 +68,17 @@ fn largest<T: PartialOrd>(list: &[T]) -> &T {
 
     largest
 }
+
+fn largest2<T>(list: &[T]) -> &T
+    where T: PartialOrd
+{
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
