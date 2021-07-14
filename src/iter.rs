8e2544107c5343d11711580ba289428c1e1ab61d
linqh <qihua.lin@quantil.com>
2021-07-14 22:16:56 +0800

    iter示例

--- src/iter.rs
+++ src/iter.rs
@@ -20,4 +20,10 @@ pub fn iterator_demonstration() {
     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
     assert_eq!(v2, vec![2, 3, 4]);
 
+    let arr = 12..15;
+    println!("range:{:?}",arr);
+    for a in arr {
+        println!("{}",a);
+    }
+
 }
