2d9561fa16bf0d04ce51e3b99a8b6d744c7dddb8
linqh <qihua.lin@quantil.com>
2021-10-02 21:27:46 +0800

    httpclient

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use myrust::*;
 use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::fs::File;
+use std::time::Duration;
+use std::str::FromStr;
+use std::net::SocketAddr;
 
 mod common;
 mod args;
@@ -7,8 +12,14 @@ mod concurrent;
 mod duotai;
 mod http;
 
-fn main() {
-    // lib_print();
-    // concurrent::modify_value_multi_thread();
-    http::http_listen();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let httpClient = reqwest::ClientBuilder::new()
+        .connect_timeout(Duration::from_secs(20))
+        .timeout(Duration::from_secs(60))
+        .build()
+        .unwrap();
+    let resp = httpClient.get("https://ms.quantil.com").send().await?.text().await?;
+    println!("resp:{}",resp);
+    Ok(())
 }
