0da4e269b79fbd86c8e75e3fbcc036e732821a92
linqh <qihua.lin@quantil.com>
2021-10-03 09:41:41 +0800

    httpclient

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::time::Duration;
 use std::str::FromStr;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, SocketAddrV4};
+use reqwest::Proxy;
 
 mod common;
 mod args;
@@ -17,9 +18,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let httpClient = reqwest::ClientBuilder::new()
         .connect_timeout(Duration::from_secs(20))
         .timeout(Duration::from_secs(60))
+        .connection_verbose(true)
+        .proxy(Proxy::http("http://127.0.0.1:1080").unwrap())
         .build()
         .unwrap();
-    let resp = httpClient.get("https://ms.quantil.com").send().await?.text().await?;
+    let resp = httpClient.get("https://ms.quantil.com/api/v2").send().await?.text().await?;
     println!("resp:{}",resp);
     Ok(())
 }
